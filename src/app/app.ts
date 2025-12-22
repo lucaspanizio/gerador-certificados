ed0f1fc05045c102e12916b04e613d1b1924aa43
Lucas Panizio <lucas_panizio@hotmail.com>
2025-12-22 20:31:57 -0300

    :sparkles: feat: add comp item certificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ItemCertificado } from '../components/item-certificado';
 import { Navbar } from '../components/navbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, ItemCertificado],
 })
 export class App {
   protected readonly title = signal('gerador-certificados');
---
+++ src/components/item-certificado/index.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from "../button";
+
+@Component({
+  selector: 'app-item-certificado',
+  templateUrl: './template.html',
+  styleUrl: './styles.css',
+  imports: [Button],
+})
+export class ItemCertificado {}
