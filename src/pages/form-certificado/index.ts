73d5a0eeac73b6f7b92826fe2a83de9bf9939773
Lucas Panizio <lucas_panizio@hotmail.com>
2025-12-28 16:17:58 -0300

    :sparkles: feat: validate form and manage activities list

--- src/pages/form-certificado/index.ts
+++ src/pages/form-certificado/index.ts
@@ -1,12 +1,38 @@
+import { FormsModule, NgModel } from '@angular/forms';
 import { Component } from '@angular/core';
+
 import { Button } from '../../components/button';
 
 @Component({
   selector: 'app-form-certificado',
   templateUrl: './template.html',
   styleUrl: './styles.css',
-  imports: [Button],
+  imports: [Button, FormsModule],
 })
 export class FormCertificado {
-  atividades = ['Atividade 1', 'Atividade 2', 'Atividade 3'];
+  name: string = '';
+  activity: string = '';
+
+  activities: string[] = ['Atividade 1', 'Atividade 2', 'Atividade 3'];
+  isDuplicatedActivity: boolean = false;
+
+  addActivity() {
+    const value = this.activity.trim();
+    if (!value) return;
+
+    const alreadyExists = this.activities.some((a) => a.toLowerCase() === value.toLowerCase());
+
+    if (alreadyExists) {
+      this.isDuplicatedActivity = true;
+      return;
+    }
+
+    this.activities.push(value);
+    this.isDuplicatedActivity = false;
+    this.activity = '';
+  }
+
+  removeActivity(index: number) {
+    this.activities.splice(index, 1);
+  }
 }
